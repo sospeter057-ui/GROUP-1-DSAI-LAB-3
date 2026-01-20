c39b36f40b2bf1bc735d9408de813d7aee7abb33
sospeter057-ui <sospeter057@gmail.com>
2026-01-20 17:12:44 +0300

    question3.py

--- question3.py
+++ question3.py
@@ -1,16 +1,20 @@
-def student_info(name, age, grade, *args, **kwargs):
-    print("Name:", name)
+def student_info(name, age, grade, *middle_last,**subjects):
+    print("Name:", name,middle_last)
     print("Age:", age)
     print("Grade:", grade)
+    print("subjects",subjects)
 
-    if args:
-        print("Additional Info (args):", args)
 
-    if kwargs:
-        print("Additional Details (kwargs):")
-        for key, value in kwargs.items():
+
+    if middle_last:
+        print("Additional Info (middle last):", middle_last)
+
+    if subjects:
+        print("Additional Details (subjects):")
+        for key, value in subjects.items():
             print(f"  {key}: {value}")
 
+student_info("loreen",20,"A","Lyimo","jay",AI="a",P="b")
     
     
     
@@ -21,5 +25,5 @@ def student_info(name, age, grade, *args, **kwargs):
     
     
     
-    
+
     
