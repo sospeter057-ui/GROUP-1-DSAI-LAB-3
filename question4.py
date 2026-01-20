5f02873b96529fcf177e29e2bf91896171e7bf4c
sospeter057-ui <sospeter057@gmail.com>
2026-01-20 17:43:03 +0300

    question4.py

--- question4.py
+++ question4.py
@@ -10,7 +10,9 @@ print("Before change_number:", num) # before running the function x was 20
 change_number(num)
 
 print("After change_number:", num) # after running the function x became 100
-
+'''
+after changing the assinged value in the function that was x=10, to the assinged num=20
+then also the printed value after changing assinged variable was no longer 100 but it was 20
 
 numbers = [2, 4, 5]
 print("Before change_list:", numbers) # before changing the list first number in the list was 2
@@ -18,3 +20,7 @@ print("Before change_list:", numbers) # before changing the list first number in
 change_list(numbers)
 
 print("After change_list:", numbers) # after changing the list first number in the list became 999
+'''
+After changing the function of change_list , value that was assigned in the function with index 0,
+after the change, printed value also changed from index 0 to 999
+'''
